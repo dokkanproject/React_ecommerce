9b8d03271737d08ccfabc5396da7abce7e2d58a4
Maxspawn <massimo.gagliardi@gmail.com>
2025-03-08 11:23:32 -0300

    Se movieron las carpetas de Json y las imagenes de Productos a Public

--- src/components/ItemListConainer.jsx
+++ src/components/ItemListConainer.jsx
@@ -14,7 +14,7 @@ const ListadoItems = () => {
   useEffect(() => {
     const fetchProductos = async () => {
       try {
-        const response = await fetch("/assets/data/productos.json");
+        const response = await fetch("/data/productos.json");
         if (!response.ok) throw new Error("No se pudo cargar el archivo JSON");
         
         const data = await response.json();
